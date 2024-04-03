9dc10912d969000cc5f22f3cb0749cb69300c196
ortizpablos <ortizpablos@gmail.com>
2024-04-03 10:12:58 -0500

    cambnio de index a src

--- index.js
+++
@@ -1,7 +0,0 @@
-import app from "./src/app/app.js";
-import { PORT } from "./src/config/config.js"
-
-app.listen(PORT);
-
-console.log("Server on port", PORT);
-
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import app from "./app/app.js";
+import { PORT } from "./config/config.js"
+
+app.listen(PORT);
+
+console.log("Server on port", PORT);
+
